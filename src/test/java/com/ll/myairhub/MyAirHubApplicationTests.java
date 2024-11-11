8a92f71ce769390173bcf3e9cb4893a629953af1
potato0221 <wnqo10@naver.com>
2024-11-11 20:43:50 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/ll/myairhub/MyAirHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.myairhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAirHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
